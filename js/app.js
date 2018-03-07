dcc072d49ae9a507fced68ededda654ba1fb8f73
ebegoflo <ebenezergf@gmail.com>
2018-03-07 13:11:27 -0600

    pintando informacion

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 var button = document.getElementById("coordenadas");
 
-  button.addEventListener('click',function getLocation() {
+  button.addEventListener('click',function getLocation(e) {
       if (navigator.geolocation) {
-          navigator.geolocation.getPosition(showPosition);
+          navigator.geolocation.getCurrentPosition(getPosition);
       } else {
         button.innerHTML = "Geolocation is not supported by this browser.";}
       });
@@ -15,15 +15,23 @@ var button = document.getElementById("coordenadas");
       $.ajax({
         url: `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/f5f581cf3ec6fc06860ecff143e9c800/${latitude},${longitude}`,
         type: 'GET',
-      }).done(jsonCall).fail(function() {
+      }).done(paintClima).fail(function() {
             console.log("Your second API call blew it.");
           });
 
   };
 
-  function jsonCall(json) {
-    console.log(json);
+  function paintClima(json) {
+    let climaContainer = document.getElementById('climaDailyContainer');
+    const infoClima = json.currently;
+    console.log(infoClima);
+
+    let templete = `<div> Temperatura ${json.currently.apparentTemperature}</div>
+    <div> Humedad ${json.currently.humidity}</div>
+    <div> RayosUv ${json.currently.uvIndex}</div>
+    <div>Presion ${json.currently.pressure}</div>`
+    climaContainer.innerHTML += templete;
   }
 
 
-  $(document).ready();
+  // $(document).ready();
